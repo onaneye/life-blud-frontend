d9ba92e49ac0bbb22d665a53bfb42a96b0ca953b
onaneye adedire <onaneyeayodeji@gmail.com>
2024-06-14 04:41:04 +0100

    initated the services data

--- lifebluds/app/Components/Hero.jsx
+++ lifebluds/app/Components/Hero.jsx
@@ -1,40 +1,59 @@
+import { useState } from "react";
+import Button from './Button'
+import { BloodDonationServices } from "./constant";
 
+const HeroSection = () => {
+  const [showCardComponent, setShowCardComponent] = useState(true);
 
+  function handleCardComponent() {
+    setShowCardComponent((prevState) => !prevState);
+  }
 
-
-// components/HeroSection.js
-const HeroSection = () => (
-    <div className="flex items-center justify-center h-full lg:h-[fit-content]  w-full lg:w-1/2 bg-gray-200 py-[100px] px-1">
-      <div className="transparent-card bg-transparent">
-        <h6 className="text-[34px] md:text-[32px] lg:text-[40px] font-bold leading-tight md:leading-snug lg:leading-[57.6px]">
-          Save Lives with a Single Donation
-        </h6>
-        <p className="text-[15px] md:text-[13px] font-bold leading-tight md:leading-normal py-4">
-          Your blood donation can save up to three lives. Join our community of lifesavers and make a difference today.
-        </p>
-        <hr className="border-gray-300 mb-8 h-1 my-5 bg-gray-300" />
-        <div>
-          <h2 className="text-[30px] md:text-[35px] font-bold leading-tight md:leading-snug lg:leading-[38.4px]">Why Donate Blood?</h2>
-          {Array(3).fill().map((_, index) => (
-            <div key={index} className="my-4">
-              <div className="flex items-center mb-0">
-                <div className="p-1 rounded-full">
-                  <img src="./icons/icon-heart.png" alt="" />
+  return (
+    <>
+      {showCardComponent ? (
+        <div className="flex items-center justify-center h-full lg:h-[fit-content]  w-full lg:w-1/2 bg-gray-200 py-[100px] px-1">
+          <div className="transparent-card bg-transparent">
+            <h6 className="text-[34px] md:text-[32px] lg:text-[40px] font-bold leading-tight md:leading-snug lg:leading-[57.6px]">
+              Save Lives with a Single Donation
+            </h6>
+            <p className="text-[15px] md:text-[13px] font-bold leading-tight md:leading-normal py-4">
+              Your blood donation can save up to three lives. Join our community of lifesavers and make a difference today.
+            </p>
+            <hr className="border-gray-300 mb-8 h-1 my-5 bg-gray-300" />
+            <div>
+              <h2 className="text-[30px] md:text-[35px] font-bold leading-tight md:leading-snug lg:leading-[38.4px]">Why Donate Blood?</h2>
+              {BloodDonationServices.map((item, index) => (
+                <div key={index} className="my-4">
+                  <div className="flex items-center mb-0">
+                    <div className="p-1 rounded-full">
+                      <img src={item.img} alt={item.img} />
+                    </div>
+                    <h3 className="text-[15px] md:text-[17px]  font-medium leading-6 md:leading-10 text-gray-800 ml-1">
+                      {item.heading}
+                    </h3>
+                  </div>
+                  <p className="text-[12px] md:text-[14px] font-medium my-0 ml-12">
+                    {item.content}
+                  </p>
                 </div>
-                <h3 className="text-[15px] md:text-[17px]  font-medium leading-6 md:leading-10 text-gray-800 ml-1">Life-Saving Impact</h3>
-              </div>
-              <p className="text-[12px] md:text-[14px] font-medium my-0 ml-12">
-                Your donation can help trauma patients, those undergoing surgery, and individuals battling cancer.
-              </p>
+              ))}
             </div>
-          ))}
+            <div className="items-center justify-center lg:hidden">
+              <button className="button-primary-lg md:self-center" onClick={handleCardComponent}>
+                Sign Up
+              </button>
+              <p className="py-3 text-center font-bold">Signed up? Login</p>
+            </div>
+          </div>
         </div>
-        <div className="items-center justify-center lg:hidden">
-          <button className="button-primary-lg md:self-center">Sign Up</button>
-          <p className="py-3 text-center font-bold">Signed up? Login</p>
-        </div>
-      </div>
-    </div>
+      ) : (
+        <>
+        <Button />
+        </>
+      )}
+    </>
   );
-  
-  export default HeroSection;
+};
+
+export default HeroSection;
--- lifebluds/app/Components/Navbar.jsx
+++ lifebluds/app/Components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
-    <div className='container px-4 py-5 my-0 flex justify-between absolute'>
-      <img src="./logo.png" alt="" className='ml-4' />
-      <button className='button-primary-sm'>Login</button>
+    <div className='container px-4 mx-4 items-center py-5 my-0 flex justify-between absolute'>
+      <img src="/logo.png" alt="Logo" width={100} height={50} />
+      <button className='hidden lg:block button-primary-sm'>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
