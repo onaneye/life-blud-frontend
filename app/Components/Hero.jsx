b701303beb7ad25b814322cc2867b9f584c0f684
onaneyeayodeji <onaneyeayodeji@gmail.com>
2024-06-16 03:05:20 +0100

    changes

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
   return (
     <>
       {showCardComponent ? (
-        <div className="flex items-center justify-center h-full lg:h-[fit-content] w-full lg:w-1/2 py-10 lg:py-20 px-4 blur-background" style={{ backgroundImage: "url('../hero-img.png')", backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: "center" }}>
+        <div className="flex items-center justify-center h-full lg:h-[fit-content] w-full lg:w-full py-10 lg:py-20 px-4 blur-background" style={{ backgroundImage: "url('../hero-img.png')", backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPosition: "center" }}>
           <div className="transparent-card bg-red-800 shadow-md bg-opacity-35 py-10 lg:py-8 px-4 text-white">
             <h6 className="text-3xl md:text-3xl lg:text-4xl font-bold leading-tight md:leading-snug lg:leading-tight font-gilroy-bold">
               Save Lives with a Single Donation
@@ -25,22 +25,22 @@ const HeroSection = () => {
             </p>
             <hr className="border-gray-300 mb-8 h-1 my-5 bg-gray-300" />
             <div>
-              <h2 className="text-2xl md:text-xl lg:text-2xl font-bold leading-tight md:leading-snug lg:leading-tight">
+              <h2 className="text-2xl md:text-xl lg:text-2xl font-bold leading-tight md:leading-snug lg:leading-tight font-gilroy-bold">
                 Why Donate Blood?
               </h2>
               {BloodDonationServices.map((item, index) => (
                 <div key={index} className="my-4">
-                  <div className="flex items-center mb-2">
-                    <div className="rounded-full">
-                      <img src={item.img} alt={item.heading} className="h-10 w-10 md:h-12 md:w-12 rounded-full" />
+                  <div className="flex">
+                    <div className="lg:min-h-12 min-w-12">
+                      <img src={item.img} alt={item.heading} className="md:h-12 md:w-12 rounded-full" />
                     </div>
-                    <h3 className="text-sm md:text-base font-medium leading-6 md:leading-8 text-white-800 ml-0">
-                      {item.heading}
+                    <h3 className="text-md md:text-base font-medium  text-white-800 ml-0 leading-relaxed font-helvetica py-2">
+                      {item.heading} <br/>{item.content}
                     </h3>
                   </div>
-                  <p className="text-xs md:text-sm font-medium my-0 ml-12">
+                  {/* <p className="text-xs md:text-sm font-medium my-0 ml-12">
                     {item.content}
-                  </p>
+                  </p> */}
                 </div>
               ))}
             </div>
